6427151ee00d000339243f249c22a83ae2e97ffd
saurav ghosh <josesaurav@gmail.com>
2021-06-09 17:39:28 +0600

    image gallery almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ImageCard from "./Components/ImageCard";
+import ImageSearch from "./Components/ImageSearch";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [term, setTerm] = useState("");
+
+    useEffect(() => {
+        fetch(
+            `https://pixabay.com/api/?key=22004522-b699d2054513c067e6675ee87&q=${term}&image_type=photo&pretty=true`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setImages(data.hits);
+                setIsLoading(false);
+            })
+            .catch((err) => console.log(err));
+    }, [term]);
+    return (
+        <div className="container mx-auto">
+            <ImageSearch searchText={(text) => setTerm(text)} />
+            {!isLoading && images.length === 0 && (
+                <h1 className="text-5xl text-center max-auto mt-32 text-blue-500">
+                    Image Not Found
+                </h1>
+            )}
+
+            {isLoading ? (
+                <h1 className="text-6xl text-center max-auto mt-32 text-blue-500">
+                    Loading...
+                </h1>
+            ) : (
+                <div className="grid grid-cols-3 gap-4">
+                    {images.map((image) => (
+                        <ImageCard key={image.id} image={image} />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
